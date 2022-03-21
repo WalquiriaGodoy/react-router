2f126ba7c315d8c5294139a1d97ccfca0e2c3b2b
Walquiria Godoy <walquiriagodoy@gmail.com>
2022-03-20 22:45:19 -0300

    feat(paths): Create dynamic paths using useRouteMatch hook

--- petshop/src/paginas/Categoria.jsx
+++ petshop/src/paginas/Categoria.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import { Route, useParams, useRouteMatch } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Route, useParams, useRouteMatch, Link } from 'react-router-dom';
 import '../assets/css/blog.css';
 import ListaCategorias from '../Components/ListaCategorias';
 import ListaPost from '../Components/ListaPost';
+import { busca } from '../api/api';
+import SubCategoria from '../paginas/SubCategoria';
+import { Switch } from 'react-router-dom';
 
 const Categoria = () => {
   const { id } = useParams();
-  const { path } = useRouteMatch();
+  const { url, path } = useRouteMatch();
+  const [subcategorias, setSubCategorias] = useState([]);
+
+  useEffect(() => {
+    busca(`/categorias/${id}`, (categoria) => {
+      setSubCategorias(categoria.subcategorias);
+    });
+  }, [id]);
+
   return (
     <>
       <div className='container'>
@@ -14,9 +25,24 @@ const Categoria = () => {
       </div>
 
       <ListaCategorias />
-      <Route exact path={`${path}/`}>
-        <ListaPost url={`/posts?categoria=${id}`} />
-      </Route>
+      <ul className='lista-categorias container flex'>
+        {subcategorias.map((subcategoria) => (
+          <li
+            className={`lista-categorias__categoria lista-categorias__categoria--${id}`}
+            key={subcategoria}
+          >
+            <Link to={`${url}/${subcategoria}`}>{subcategoria}</Link>
+          </li>
+        ))}
+      </ul>
+      <Switch>
+        <Route exact path={`${path}/`}>
+          <ListaPost url={`/posts?categoria=${id}`} />
+        </Route>
+        <Route path={`${path}/:subcategoria`}>
+          <SubCategoria />
+        </Route>
+      </Switch>
     </>
   );
 };
